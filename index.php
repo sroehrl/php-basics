69a0b3cb1e61ce2f427c737420e4dc682c90d420
sroehrl <stefan.roehrl@gmail.com>
2020-04-22 10:59:06 -0400

    Add database & env handling

--- index.php
+++ index.php
@@ -9,6 +9,23 @@ use Neoan3\Apps\Template;
 
 define('path',__DIR__);
 
+/*
+*   SETUP
+ * */
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+try{
+    \Neoan3\Apps\Db::setEnvironment([
+        'host' => getenv('DB_HOST'),
+        'name' => getenv('DB_NAME'),
+        'user' => getenv('DB_USER'),
+        'password' => getenv('DB_PASSWORD')
+    ]);
+} catch (\Neoan3\Apps\DbException $e){
+    die('Database issue');
+}
+
+
 $title = 'Homepage';
 
 if(isset($_GET['route'])){
